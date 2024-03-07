a42be4eb0fb41690edefb5d0ae4049c44fe626a0
Julie Katz <98181307+jkatz10@users.noreply.github.com>
2024-03-07 13:31:42 -0500

    Update PitStop

--- PitStop.py
+++ PitStop.py
@@ -4,11 +4,8 @@ from geometry_msgs.msg import Twist
 import sys
 import requests
 import json
-
 URL = ' https://api.airtable.com/v0/appNjaGBkp373VxXa/Tasks'
 Headers = {'Authorization':'Bearer patLKfBRrXM982KBX.8a4a2b6d6124cf86fec4f8247e5fd5bd83f63665c1abce948f6afaf73d435533'}
-
-
 class PublisherNode(Node):
     def __init__(self):
         super().__init__('publisher_node')
@@ -16,17 +13,32 @@ class PublisherNode(Node):
     def publish_velocities(self):
         r = requests.get(url = URL, headers = Headers, params = {})
         data = r.json()
-        file_path = 'airtable_data.json'   
+        file_path = 'airtable_data.json'
         records = data.get('records', [])
-        linear_velocity = data['records'][3]['fields']['Values']
-        angular_velocity = data['records'][1]['fields'] ['Values']
+        try:
+            linear_velocity = data['records'][1]['fields']['Values']
+            angular_velocity = data['records'][0]['fields'] ['Values']
+        except KeyError:
+            linear_velocity = 0
+            angular_velocity = 0
+        try:
+            linear_velocity = float(linear_velocity)
+            angular_velocity = float(angular_velocity)
+        except ValueError or KeyError:
+            linear_velocity = 0
+            angular_velocity = 0
         twist = Twist()
-        linDist = linear_velocity
-        twist.linear.x = float(linDist)
+        try:
+            linDist = linear_velocity
+            twist.linear.x = linDist
+            angDist = angular_velocity
+            twist.angular.z = angDist
+        except AssertionError:
+            linDist = float(0)
+            twist.linear.x = float(0)
+            angDist = float(0)
+            twist.angular.z = float(0)
         print(twist.linear.x)
-        # Set the angular velocity
-        angDist = angular_velocity
-        twist.angular.z = float(angDist)
         print(twist.angular.z)
         self.publisher_.publish(twist)
         #return cont
